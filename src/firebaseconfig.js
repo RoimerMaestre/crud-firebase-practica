f6ef6ac92ee05d2a845111ebd0b87e1336eea68b
RoimerMaestre <roimer.maestre93@gmail.com>
2024-07-30 12:54:50 -0400

    practica firebase

---
+++ src/firebaseconfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC0MmbbOPnxaP87uRJ5AYsG61d_alITIaQ",
+  authDomain: "crud-colaborador-50bf6.firebaseapp.com",
+  projectId: "crud-colaborador-50bf6",
+  storageBucket: "crud-colaborador-50bf6.appspot.com",
+  messagingSenderId: "940200473000",
+  appId: "1:940200473000:web:8f76e371384bf0e26aad9d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
